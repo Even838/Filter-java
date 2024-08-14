d5a08185a66e772fa3112cb2c4c736f9ef6e5313
Even-dev <samuca1234.oficial@gmail.com>
2024-08-14 10:52:13 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,20 @@
+
+public class main {
+
+  public static void main(String[] args) {
+   
+   
+   int[] arrew = {12,-5,10,8,-6};
+   
+   
+   for (int i = 0; i < 5; i++) {
+    if (arrew[i] > 0) {
+        System.out.println(arrew[i]);
+    }
+}
+
+    
+   
+   
+  }
+}
